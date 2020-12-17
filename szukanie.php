5aff0271116bbedc0afab957ed1136eaf9824246
KubaNocon7890 <42960635+KonneckY@users.noreply.github.com>
2020-12-17 11:59:18 +0100

    Add files via upload

--- szukanie.php
+++ szukanie.php
@@ -15,13 +15,13 @@
         <a class="github-logo" href="https://github.com/3ti-2020/crud-wiele-do-wielu-kuba-nocon">
     <img class="github-logo" src="github.png" alt="github.com - odnośnik">
     </a>
-        <a class="logo" href="https://musicblogapp.herokuapp.com/"><h1 class="titlesite">Music Information Blog</h1></a>     
+         <a class="logo" href="https://musicblogapp.herokuapp.com/"><h1 class="titlesite">Music Information Blog</h1></a>   
         </div>
         <div class="main">
             <?php
                 $conn = new mysqli("remotemysql.com", "Q5ce6IILxv", "d8rRn6imdt", "Q5ce6IILxv");
 
-                $result1 = $conn->query("SELECT * FROM post");
+                $result1 = $conn->query("SELECT DISTINCT * FROM post JOIN post_tag ON post.id_post=post_tag.id_post JOIN tag ON post_tag.id_tag=tag.id_tag WHERE tag.tagname ='".$_POST['szukaj_tag']."'");
                 while($row1 = $result1->fetch_assoc() ){
                     echo("<article class='post'>");
                     echo("<h2 class='tytul'>".$row1['tytul']."</h2>");
@@ -29,14 +29,17 @@
                     echo("<p class='tresc'>");
                     echo($row1['tresc']);
                     echo("</p>");
+                    echo("<div class='tagi'>");
                     echo("<h3 class='tag'>");
-                        $result2 = $conn->query("SELECT * FROM tag JOIN post_tag ON tag.id_tag=post_tag.id_tag WHERE post_tag.id_post=".$row1['id_post']);
+                        $result2 = $conn->query("SELECT * FROM tag JOIN post_tag ON tag.id_tag=post_tag.id_tag WHERE post_tag.id_post='".$row1['id_post']."'");
                         while($row2 = $result2->fetch_assoc()){
-                            echo("<form action='szukanie.php' method='post' class='form_tag'>
-                                <input type='submit' class='in_tag' name='szukaj_tag' value='".$row2['tagname']."'>
+                            echo("<form action='szukanie_tagu.php' method='post' class='form_tag'>
+                                 <input type='submit' class='in_tag' name='szukaj_tag' value='".$row2['tagname']."'>
                                 </form>");
                         }
+                        
                     echo("</h3>");
+                    echo("</div>");
                     echo("</article>");
                 }
 
